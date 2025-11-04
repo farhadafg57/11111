28d15377aadf5e2ebc20914749df59441bf53412
Farhad Sakhi <forextraderafg@gmail.com>
2025-11-04 14:26:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9648397962-1720d",
+  "appId": "1:629578832931:web:389239baeffc1840c8a596",
+  "apiKey": "AIzaSyDZHmynBAMKKc0Api6215_tJdBvndw0t0I",
+  "authDomain": "studio-9648397962-1720d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "629578832931"
+};
