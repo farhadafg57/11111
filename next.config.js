9932a1a8b8f562f2696a9268f87c96dc8c38324f
Farhad Sakhi <forextraderafg@gmail.com>
2025-11-08 22:11:55 +0000

    Core Technology: Three.js / React Three Fiber is mandatory. This UI cann

--- next.config.js
+++ next.config.js
@@ -8,6 +8,49 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    });
+    return config;
+  },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '4mb',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+  // This is required for Drei's <Html> component to properly render SVGs from lucide-react
+  // by allowing foreignObject in the SVG output.
+  webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test && rule.test.test('.svg')
+    );
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/;
+    }
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  svgs: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "style-src 'unsafe-inline'",
+  },
 };
 
 export default nextConfig;
